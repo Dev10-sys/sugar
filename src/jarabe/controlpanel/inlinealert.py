f24fcace52303002480182dd8b8bf146d245c7d6
Dev10-sys <kalpanagola9897@gmail.com>
2026-06-04 23:58:21 +0530

    Port Control Panel to GTK4

--- src/jarabe/controlpanel/inlinealert.py
+++ src/jarabe/controlpanel/inlinealert.py
@@ -41,7 +41,6 @@ class InlineAlert(Gtk.Box):
     }
 
     def __init__(self, **kwargs):
-
         self._msg = None
         self._msg_color = None
         self._icon = Icon(icon_name='emblem-warning',
@@ -51,20 +50,31 @@ class InlineAlert(Gtk.Box):
         self._msg_label = Gtk.Label()
         self._msg_label.set_max_width_chars(150)
         self._msg_label.set_ellipsize(style.ELLIPSIZE_MODE_DEFAULT)
-        self._msg_label.set_alignment(0, 0.5)
-        self._msg_label.modify_fg(Gtk.StateType.NORMAL,
-                                  style.COLOR_SELECTION_GREY.get_gdk_color())
+        # Gtk.Alignment removed, use halign on Label
+        self._msg_label.set_halign(Gtk.Align.START)
+        self._msg_label.set_valign(Gtk.Align.CENTER)
+        
+        # CSS to replace modify_fg
+        css_provider = Gtk.CssProvider()
+        css = "* { color: %s; }" % style.COLOR_SELECTION_GREY.get_html()
+        css_provider.load_from_data(css.encode())
+        context = self._msg_label.get_style_context()
+        context.add_provider(css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
         Gtk.Box.__init__(self, orientation=Gtk.Orientation.HORIZONTAL, **kwargs)
 
         self.set_spacing(style.DEFAULT_SPACING)
-        self.modify_bg(Gtk.StateType.NORMAL,
-                       style.COLOR_WHITE.get_gdk_color())
+        
+        css_provider_bg = Gtk.CssProvider()
+        css_bg = "* { background-color: %s; }" % style.COLOR_WHITE.get_html()
+        css_provider_bg.load_from_data(css_bg.encode())
+        context_bg = self.get_style_context()
+        context_bg.add_provider(css_provider_bg, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
-        self.pack_start(self._icon, False, False, 0)
-        self.pack_start(self._msg_label, False, False, 0)
-        self._msg_label.show()
-        self._icon.show()
+        self.append(self._icon)
+        self.append(self._msg_label)
+        self._msg_label.set_visible(True)
+        self._icon.set_visible(True)
 
     def do_set_property(self, pspec, value):
         if pspec.name == 'msg':
